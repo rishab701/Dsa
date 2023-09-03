bfbd828ff3a70770df9a4c83fc129e5a7a8fa7e2
rishab701 <109593169+rishab701@users.noreply.github.com>
2023-09-03 16:41:40 +0530

    Time: 4 ms (14.96%) | Memory: 6.5 MB (54.47%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,31 +1,20 @@
 class Solution {
-public:
-    bool isValid(int i, int j, int m, int n) {
-        return i >= 0 && i < m && j >= 0 && j < n;
-    }
-
-    int f(int i, int j, int m, int n, vector<vector<int>>& grid,vector<vector<int>>&dp) {
-        if (i == m - 1 && j == n - 1) {
-            return 1; // Reached the destination.
-        }
-
-        int count = 0;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if (isValid(i + 1, j, m, n)) {
-            count += f(i + 1, j, m, n, grid,dp);
-        }
-        if (isValid(i, j + 1, m, n)) {
-            count += f(i, j + 1, m, n, grid,dp);
-        }
-
-        return dp[i][j]=count;
-    }
-
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> grid(m, vector<int>(n, 0));
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(0, 0, m, n, grid,dp);
-    }
+  public:
+   int f(int i,int j,int m,int n,vector<vector<int>>& dp){
+      if (i==m-1 && j==n-1){
+          return 1;
+      }
+      if(i>=m || j>=n){
+          return 0;
+      }
+      if(dp[i][j]!=-1)  
+      {
+          return dp[i][j];
+      }
+    return dp[i][j]=f(i+1, j,m,n,dp) + f(i, j+1,m,n,dp);
+   }
+   int uniquePaths(int m, int n) {
+     vector<vector<int>>dp(m,vector<int>(n,-1));
+     return f(0,0,m,n,dp);
+  }
 };
