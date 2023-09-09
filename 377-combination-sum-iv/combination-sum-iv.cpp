ce1e99e539eeb9eb78ae9c18ef3e2621718fe045
rishab701 <109593169+rishab701@users.noreply.github.com>
2023-09-09 19:06:35 +0530

    Time: 3 ms (50.37%) | Memory: 6.7 MB (46.24%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int f(vector<int>& nums,int target,vector<int>& dp){
-        dp[0]=1;
+        if(target == 0){
+            return 1;
+        }
         if(dp[target]>-1) return dp[target];
         int count=0;
         for(int i=0;i<nums.size();i++){
